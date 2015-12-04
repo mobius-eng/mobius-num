51ac4e2a2d932be470000178d8cbe4515d95658e
mobius-eng <alexey.cherkaev@gmail.com>
2015-12-04 09:55:20 +0200

    updates

--- tests/ode.lisp
+++ tests/ode.lisp
@@ -9,4 +9,4 @@
                         ode-function
                         (ode-state 0d0 (vec 'double-float 1d0))
                         1d-1)
-  (ode-accept-error-p rk45ck 1d-1 (vec 'double-float 1d0) 1d-5))
+  (ode-accept-error-p rk45ck 1d-1 (vec 'double-float 1d0) 1d-8))
